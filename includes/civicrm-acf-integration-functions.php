55e83c0f887de2d192bb3b1e1297bfe871b664d9
Christian Wach <needle@haystack.co.uk>
2020-03-10 09:07:57 +0000

    Add theme function to retrieve age from an ACF Field

--- includes/civicrm-acf-integration-functions.php
+++ includes/civicrm-acf-integration-functions.php
@@ -10,3 +10,61 @@
 
 
 
+/**
+ * Get "Age" as a string for a given ACF Field.
+ *
+ * @since 0.6.2
+ *
+ * @param str $selector The ACF field selector.
+ * @param int $post_id The numeric ID of the WordPress Post.
+ * @return str $age_string The age expressed as a string.
+ */
+function cacf_get_age_from_acf_field( $selector, $post_id = null ) {
+
+	// Get reference to plugin.
+	$cacf = civicrm_acf_integration();
+
+	// Try the global if no Post ID.
+	if ( is_null( $post_id ) ) {
+		global $post;
+		if ( ! ( $post instanceof WP_Post ) ) {
+			return '';
+		}
+		$post_id = $post->ID;
+	}
+
+	// Get field settings.
+	$acf_settings = get_field_object( $selector, $post_id );
+
+	// Bail if we don't get any settings.
+	if ( empty( $acf_settings ) ) {
+		return '';
+	}
+
+	// Bail if it's not a "Date" or "Date Time" ACF Field.
+	if ( ! in_array( $acf_settings['type'], [ 'date_picker', 'date_time_picker' ] ) ) {
+		return '';
+	}
+
+	// Get Field value.
+	$value = get_field( $selector );
+
+	// Bail if it's empty.
+	if ( empty( $value ) ) {
+		return '';
+	}
+
+	// Convert ACF Field value to CiviCRM "Ymdhis" format.
+	$datetime = DateTime::createFromFormat( $acf_settings['return_format'], $value );
+	$date = $datetime->format( 'Ymdhis' );
+
+	// Get "Age" as string.
+	$age = $cacf->civicrm->contact_field->date_age_get( $date );
+
+	// --<
+	return $age;
+
+}
+
+
+
